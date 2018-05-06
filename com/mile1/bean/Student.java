3c0de27a26b1802587f719b398a505b3b89306bb
Shubhdeep Singh <singh18shubhdeep@gmail.com>
2018-05-06 23:44:10 +0530

    Third Commit

--- com/mile1/bean/Student.java
+++ com/mile1/bean/Student.java
@@ -1,5 +1,31 @@
 package com.mile1.bean;
 
 public class Student {
+	String name;
+	int marks[];
+	
+	public Student(){
+		marks=new int[3];
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int[] getMarks() {
+		return marks;
+	}
+
+	public void setMarks(int[] marks) {
+		this.marks = marks;
+	}
+	
+	
+	
+	
+	But i have changed the file
 }
